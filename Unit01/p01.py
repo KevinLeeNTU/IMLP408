08121b8d0a06482b74cb594fc26b4ee6b7366b34
KevinLeeNTU <162401462+KevinLeeNTU@users.noreply.github.com>
2024-03-13 14:53:15 +0800

    Finish Unit01

---
+++ Unit01/p01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 17:21:47 2024
+
+@author: User
+"""
+
+import Mytime
+
+t1=666
+
+print(Mytime.GetTime("%Y-%m-%d %H:%M:%S"))
+print(Mytime.t1)
+print(t1)
+print(Mytime.time.time())
